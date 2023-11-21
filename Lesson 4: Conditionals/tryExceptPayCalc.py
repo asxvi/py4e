227e02e4ff14bf715faa30d1094896d4705b4e87
asxvi <143113267+asxvi@users.noreply.github.com>
2023-11-21 12:14:13 -0600

    Add files via upload

---
+++ Lesson 4: Conditionals/tryExceptPayCalc.py
@@ -0,0 +1,25 @@
+
+try:
+    hours = float(input('Enter Hours: '))
+    rate = float(input('Enter Rate: '))
+    
+except:
+    print('error please enter numeric input')
+    quit()
+
+if(hours>=40):
+    weekPay = (rate*40) + ((hours-40)*(1.5*rate))
+    monthPay = weekPay * 4
+    yearPay = weekPay * 46
+    print('Weekly Pay: ', weekPay)
+    print('Monthly Pay: ', monthPay)
+    print('Yearly Pay: ', yearPay)
+
+
+else:
+    weekPay = float(hours) * float(rate)
+    monthPay = weekPay * 4
+    yearPay = weekPay * 46
+    print('Weekly Pay: ', weekPay)
+    print('Monthly Pay: ', monthPay)
+    print('Yearly Pay: ', yearPay)
